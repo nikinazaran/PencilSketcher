b50c8f0a7d7f5d92e038046f586ae78f0a2eb687
Niki Nazaran <niki13sh@gmail.com>
2020-09-29 11:14:51 +0330

    Using args to pass on file names.

--- Sketcher.py
+++ Sketcher.py
@@ -1,6 +1,7 @@
 import matplotlib.image as img
 import matplotlib.pyplot as plt
 import cv2
+import argparse
 
 
 def show_image(img):
@@ -8,7 +9,13 @@ def show_image(img):
     plt.show()
 
 
-orig = img.imread("img.jpg")
+parser = argparse.ArgumentParser("sketcherrrr")
+parser.add_argument("InputImage", help="Output image name.")
+parser.add_argument("-o", help="Output image name.", default='final.jpg')
+args = parser.parse_args()
+params = vars(args)
+
+orig = img.imread(params.get('InputImage'))
 # show_image(orig)
 grayScaled = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
 # show_image(grayScaled)
@@ -18,4 +25,4 @@ blurred = cv2.GaussianBlur(inverted, (23, 23), sigmaX=20, sigmaY=20)
 # show_image(blurred)
 finalResult = cv2.divide(grayScaled, 255 - blurred, scale=256)
 # show_image(finalResult)
-plt.imsave('FinalResult.png', finalResult, cmap='gray', vmin=0, vmax=255)
+plt.imsave(params.get('o'), finalResult, cmap='gray', vmin=0, vmax=255)
